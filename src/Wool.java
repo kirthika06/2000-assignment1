41c510ebe610a22b16ccc35b6e2e2a437fd757ff
Kirthika Nadimuthu <kirthikanadimuthu@Kirthikas-MacBook-Air-2.local>
2025-09-18 22:25:27 +1000

    Add wool item for cat

---
+++ src/Wool.java
@@ -0,0 +1,11 @@
+public class Wool extends Item {
+
+    public Wool(){
+        super("Wool");
+    }
+
+    public void interact(){
+        System.out.println("The cat is playing with the wool!");
+    }
+    
+}
