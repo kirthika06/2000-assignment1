7a45ea71971b76be280dee5f324c318dc37fedce
Kirthika Nadimuthu <kirthikanadimuthu@Kirthikas-MacBook-Air-2.local>
2025-09-19 20:44:20 +1000

    Added Meat.java

---
+++ src/Meat.java
@@ -0,0 +1,25 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+public class Meat extends Item implements PickItem {
+
+    public Meat(Cell loc) {
+        super(loc, "Meat");
+    }
+
+    public void paint(Graphics g){
+        g.setColor(Color.RED);
+        g.fillOval(getLoc().x +8, getLoc().y + 8, 18,12);
+
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.PLAIN, 10));
+    }
+
+    public void pick(Actor actor){
+        System.out.println("The dog is eating the meat!");
+    }
+
+ 
+
+}
