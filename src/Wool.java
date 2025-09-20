8d6d362000491e9fd33eee09e08c78b32110359e
Kirthika Nadimuthu <kirthikanadimuthu@Kirthikas-MacBook-Air-2.local>
2025-09-20 20:53:39 +1000

    added iteraction between cat and wool

--- src/Wool.java
+++ src/Wool.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 
-public class Wool extends Item {
+public class Wool extends Item implements PickItem {
 
         public Wool(Cell loc) {
              super(loc, "Wool");
