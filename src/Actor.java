1d075498b57b2da5dc2e0c6a638069a8aab1907c
Kirthika Nadimuthu <kirthikanadimuthu@Kirthikas-MacBook-Air-2.local>
2025-09-21 19:45:55 +1000

    fixed minor errors

--- src/Actor.java
+++ src/Actor.java
@@ -6,11 +6,15 @@ public abstract class Actor {
   ArrayList<Polygon> shapes = new ArrayList<>();
   Cell loc;
 
+
   public void paint(Graphics g) {
+
     for (Polygon P : shapes){
       g.drawPolygon(P); //outline
       g.fillPolygon(P); //Filling
     }
+
+    
   }
 }
 
--- src/PickItem.java
+++ src/PickItem.java
@@ -2,3 +2,4 @@ public interface PickItem {
 
     boolean pickedBy (Actor actor);
 }
+
